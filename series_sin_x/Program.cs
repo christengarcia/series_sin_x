72a54d9e9b1f78154e438ab9cfb6fd554744a527
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-02-18 07:36:40 +1100

    Initial commit

---
+++ series_sin_x/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * C# Program to calculate the series sin(x)=x-x^3/3!+x^5/!-x^7/7!+......
+ */
+
+using System;
+
+namespace series_sin_x
+{
+    class Sine
+    {
+        int deg, n;
+        public void ReadData()
+        {
+            Console.WriteLine("Enter the Number of Terms:");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Angle in Degrees:");
+            deg = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
